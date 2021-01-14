95e286dcd92a437c96c5cac2100b50d6b0e1d792
georgerapeanu <alexandrurapeanu@yahoo.com>
2021-01-14 11:48:37 +0200

    adds driver promotion and disconnection

--- Client.py
+++ Client.py
@@ -8,28 +8,41 @@ import Gamepad;
 
 #TODO resend message after a certain time because of udp shit and packet loss 
 
+def handle_server_messages(driver):
+    while True:
+        msg = driver.recv(__env.MAX_MESSAGE_SIZE).decode(__env.FORMAT);
+        print(msg);
+
 def start():
     gamepad = Gamepad.Gamepad();
 
     driver = socket.socket(socket.AF_INET,socket.SOCK_STREAM);
     driver.connect(__env.HOST);
+
+
     connect_msg = driver.recv(__env.MAX_MESSAGE_SIZE).decode(__env.FORMAT);
 
     print(connect_msg);
     
     if connect_msg in __env.ERROR_MESSAGES:
         return ;
+    
+    thread = threading.Thread(target=handle_server_messages,args = [driver]);
+    thread.start();
+
 
     print("driver connected at ",__env.HOST);
 
     last_transmission_message = "";
+    last_time = time.time() - 20;
 
     while True:
         time.sleep(0.01);
         gamepad.update_inputs();
         message = gamepad.get_transmission_message();
-        if message != last_transmission_message:
+        if message != last_transmission_message or (time.time() - last_time >= 1):
             last_transmission_message = message;
+            last_time = time.time();
             driver.send(message.encode(__env.FORMAT));
 
 start();
--- Server.py
+++ Server.py
@@ -7,36 +7,64 @@ import threading;
 import os;
 import sys;
 
+TIMEOUT_DURATION = 2;
+
 host = socket.socket(socket.AF_INET,socket.SOCK_STREAM);
 host.bind(("0.0.0.0",__env.HOST_PORT));
 
 robot = socket.socket(socket.AF_INET,socket.SOCK_DGRAM);
+
+available_driver_spots = [False,True,True];
 
 def handle_client(conn,addr):
     print("attempted connection ",addr);
     if addr[0] not in __env.DRIVER_ADDRESSES:
         print("address not recognized, connection failed");
         conn.send(__env.ERROR_MESSAGES[1].encode(__env.FORMAT));
-
-    elif threading.activeCount() - 1 > __env.DRIVER_COUNT:
-        conn.send(__env.ERROR_MESSAGES[0].encode(__env.FORMAT));
     else:
-        driver_id = threading.activeCount() - 1;
+        driver_id = -1;
+        for i in range(0,len(available_driver_spots)):
+            if available_driver_spots[i] == True:
+                driver_id = i;
+                available_driver_spots[i] = False;
+                break;
+        if driver_id == -1:
+            conn.send(__env.ERROR_MESSAGES[0].encode(__env.FORMAT));
+            return ;
+
         print("driver " + str(driver_id) + " connected at " + str(addr));
         conn.send(str("welcome driver #" + str(driver_id)).encode(__env.FORMAT));
+        conn.settimeout(TIMEOUT_DURATION);
         connected = True;
 
         while connected:
-            msg = conn.recv(__env.MAX_MESSAGE_SIZE).decode(__env.FORMAT);
-            if len(msg):
+            try:
+                msg = conn.recv(__env.MAX_MESSAGE_SIZE).decode(__env.FORMAT);
+            except socket.timeout:
+                connected = False;
+                print("Driver " + str(driver_id) + " disconnected");
+                available_driver_spots[driver_id] = True;
+                break;
+
+            if connected == False:
+                break;
+
+            promoted = False;
+
+            for i in range(0,driver_id):
+                if available_driver_spots[i] == True:
+                    avialable_driver_spots[i] = False;
+                    avialable_driver_spots[driver_id] = True;
+                    driver_id = i;
+                    promoted = True;
+                    break;
+            if promoted:
+                conn.send(("Promoted to driver #" + str(driver_id)).encode(__env.FORMAT));
+
+            if len(msg) > 0:
                 msg = "G~" + str(driver_id) + "," + msg;
                 print("recieved following command ");
                 print(msg);
-                #if msg == __env.DISCONNECT:
-                #    connected = False;
-                #    print("Driver disconnected, shutting down");
-                #    host.shutdown(socket.SHUT_RDWR)
-                #    host.close()
                 robot.sendto(msg.encode(__env.FORMAT),__env.ROBOT);
     conn.close();
 
