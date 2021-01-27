e331643c8f866b9a72ddd6836f2abd7aa8dd3bca
georgerapeanu <alexandrurapeanu@yahoo.com>
2021-01-27 20:19:21 +0200

    Update RemoteDrive.java

--- RemoteDrive.java
+++ RemoteDrive.java
@@ -31,6 +31,7 @@ package org.firstinspires.ftc.teamcode;
 
 import android.util.Log;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
